ffe99fe9777d004fbd8d4363cc2efc149891d2aa
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-06-09 20:49:49 -0300

    feat: validating if value are numbers
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,3 +1,10 @@
-module.exports.sum = (num1, num2) => {
+module.exports.sum = (value1, value2) => {
+  let num1 = parseInt(value1, 10);
+  let num2 = parseInt(value2, 10);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    throw new Error("please check the value passed in input");
+  }
+
   return num1 + num2;
 };
