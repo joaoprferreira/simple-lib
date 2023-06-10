b746982d8d59c3fd643bc45f993a928a250214f9
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-06-10 00:21:53 -0300

    refactor: prettier
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,12 +1,12 @@
-const { queryString } = require("./queryString");
+const { queryString } = require('./queryString');
 
-describe("Object to query string", () => {
-  it("should crate a valid query string when an object is provider", () => {
+describe('Object to query string', () => {
+  it('should crate a valid query string when an object is provider', () => {
     const obj = {
-      name: "joao",
-      cargo: "SoftwareDevelopment",
+      name: 'joao',
+      cargo: 'SoftwareDevelopment',
     };
 
-    expect(queryString(obj)).toBe("name=joao&cargo=SoftwareDevelopment");
+    expect(queryString(obj)).toBe('name=joao&cargo=SoftwareDevelopment');
   });
 });
