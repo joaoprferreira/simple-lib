74ec912b528e2a50cd1e408f60b419c17ef0112c
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-06-10 00:21:12 -0300

    refactor: prettier
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -3,7 +3,7 @@ module.exports.sum = (value1, value2) => {
   let num2 = parseInt(value2, 10);
 
   if (Number.isNaN(num1) || Number.isNaN(num2)) {
-    throw new Error("please check the value passed in input");
+    throw new Error('please check the value passed in input');
   }
 
   return num1 + num2;
