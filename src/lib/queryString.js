19ef9bc5fe0d3acfb6524f5c9b1c37164d1cc40c
DESKTOP-IHOGI0G\joao <joaof082@gmail.com>
2023-06-10 00:21:42 -0300

    refactor: refactoring code
    Signed-off-by: DESKTOP-IHOGI0G\joao <joaof082@gmail.com>

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,7 +1,4 @@
-module.exports.queryString = (obj) => {
-  const response = Object.entries(obj).map((item) => {
-    return `${item[0]}=${item[1]}`;
-  });
-
-  return response.join("&");
-};
+module.exports.queryString = obj =>
+  Object.entries(obj)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
